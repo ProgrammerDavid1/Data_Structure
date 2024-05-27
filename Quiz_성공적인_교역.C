f83fffb12faf11b862c2cf157f0de96f57ba0f9f
ProgrammerDavid1 <161571242+ProgrammerDavid1@users.noreply.github.com>
2024-05-27 10:19:09 +0900

    Update Quiz_성공적인_교역.C

--- Quiz_성공적인_교역.C
+++ Quiz_성공적인_교역.C
@@ -21,33 +21,41 @@ void init(HeapType* h) {
     h->heap_size = 0;
 }
 // 현재 요소의 개수가 heap_size인 힙 h에 item을 삽입한다.
-void push_min_heap(HeapType* h, element item) {
+void push_max_heap(HeapType* h, element item) {
     int i;
     i = ++(h->heap_size);
-
-    //  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
-    while ((i != 1) && (item.benefit <= h->heap[i / 2].benefit)) {
+    // upheap
+    //트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
+    while ((i != 1) && (item.benefit > h->heap[i / 2].benefit
+        || (item.benefit == h->heap[i / 2].benefit && item.res_type < h->heap[i / 2].res_type))) {
+        // 비교 시 benefit이 같으면 res_type을 기준으로 정렬
         h->heap[i] = h->heap[i / 2];
         i /= 2;
     }
-    h->heap[i] = item;   // 새로운 노드를 삽입
+    h->heap[i] = item;    // 새로운 노드를 삽입
 }
+
 // 삭제 함수
-element pop_min_heap(HeapType* h) {
+element pop_max_heap(HeapType* h) {
     int parent, child;
     element item, temp;
 
     item = h->heap[1];
-    temp = h->heap[(h->heap_size)--];
-    parent = 1;
+    temp = h->heap[(h->heap_size)--];  parent = 1;
     child = 2;
+    // downheap
     while (child <= h->heap_size) {
-        // 현재 노드의 자식노드 중 더 작은 자식노드를 찾는다.
+        // 현재 노드의 자식노드 중 더 큰 자식노드를 찾는다.  
         if ((child < h->heap_size) &&
-            (h->heap[child].benefit) >= h->heap[child + 1].benefit)
+            (h->heap[child].benefit < h->heap[child + 1].benefit
+                || (h->heap[child].benefit == h->heap[child + 1].benefit && h->heap[child].res_type > h->heap[child + 1].res_type)))
+            // 비교 시 benefit이 같으면 res_type을 기준으로 정렬
             child++;
-        if (temp.benefit < h->heap[child].benefit) break;
-        // 한 단계 아래로 이동
+        if (temp.benefit > h->heap[child].benefit ||
+            (temp.benefit == h->heap[child].benefit && temp.res_type <= h->heap[child].res_type)) {
+            // 비교 시 benefit이 같으면 res_type을 기준으로 정렬
+            break;
+        }
         h->heap[parent] = h->heap[child];
         parent = child;
         child *= 2;
@@ -75,7 +83,7 @@ int main() {
             scanf("%d %d", &benefit1, &benefit2);
             A_benefit[res_type] = benefit1;
             B_benefit[res_type] = benefit2;
-            push_min_heap(h, { (benefit1 + benefit2), res_type });
+            push_max_heap(h, { (benefit1 + benefit2), res_type });
         }
 
         int A_benefit_sum = 0;
@@ -84,7 +92,7 @@ int main() {
 
         // 요소들을 번갈아가며 선택하며 이익을 계산
         for (int res_type = 0; res_type < resource_num; res_type++) {
-            element item = pop_min_heap(h);
+            element item = pop_max_heap(h);
             if (is_A_turn) {
                 A_benefit_sum += A_benefit[item.res_type];
             }
